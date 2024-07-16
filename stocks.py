bd02f032a6d6b77bd8f9672ec217066fa7384cb3
Jon Goose <jongoose69@gmail.com>
2024-07-15 17:18:46 -0700

    formatting

--- stocks.py
+++ stocks.py
@@ -4,6 +4,9 @@ import re
 import os
 import yfinance as yf
 from colorama import Fore
+import logging
+
+logging.getLogger('yfinance').disabled = True
 
 server = 'irc.chat.twitch.tv'
 port = 6667
@@ -18,36 +21,49 @@ sock.send(f"NICK {nickname}\n".encode('utf-8'))
 sock.send(f"JOIN {channel}\n".encode('utf-8'))
 
 
-regex = re.compile('[^a-zA-Z]')
+regex = re.compile('[^a-zA-Z-]')
 dick = {}
+period = 10
+last_printed = 0
+changed = False
 
 while True:
 	resp = sock.recv(2048).decode('utf-8')
 	#remove values older than 10 mins
-	tenago = time.time() - 600
+	now = time.time()
+	tenago = now - 600
 	for tickler in dick:
 		dick[tickler] = [ts for ts in dick[tickler] if tenago < ts]
 	for word in resp.split():
 		if "$" in word:
+			changed = True
 			tickler = regex.sub('', word).upper()
 			if tickler in dick:
 				dick[tickler].append(time.time())
 			else:
 				dick[tickler] = [time.time()]
 	
-	votes = {tickler: len(dick[tickler]) for tickler in dick}
-	votes = dict(sorted(votes.items(), key=lambda x:x[1], reverse=True))
-	os.system('clear')
-	print()
-	for tickler in votes:
-		try:
-			quote = yf.Ticker(tickler).info
-			price = (quote['bid'] + quote['ask']) / 2
-			percent_change =100 * (price - quote['previousClose'])/ quote['previousClose']
-			if percent_change > 0:
-				print(Fore.GREEN, end="")
-			else:
-				print(Fore.RED, end="")
-			print(" {:7} ${:<10.2f} {:<+3.2f}%".format(tickler, price, percent_change))
-		except:
-			pass
+
+	if now - last_printed > period and changed:
+		changed = False
+		last_printed = now
+		votes = {tickler: len(dick[tickler]) for tickler in dick}
+		votes = dict(sorted(votes.items(), key=lambda x:x[1], reverse=True))
+		os.system('clear')
+		print()
+		printed = 0
+		for tickler in votes:
+			if printed >= 10:
+				break
+			try:
+				quote = yf.Ticker(tickler).info
+				price = (quote['bid'] + quote['ask']) / 2
+				percent_change =100 * (price - quote['previousClose'])/ quote['previousClose']
+				if percent_change > 0:
+					print(Fore.GREEN, end="")
+				else:
+					print(Fore.RED, end="")
+				print(" {:5} ${:<7.2f} {:<+3.2f}%".format(tickler, price, percent_change))
+				printed += 1
+			except:
+				pass
