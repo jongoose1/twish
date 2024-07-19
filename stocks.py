7aa2c7ea86318a1a53780765ba2e792c0765edc0
Jon Goose <jongoose69@gmail.com>
2024-07-19 15:50:31 -0700

    siosdiosfnjoinjsiosdiosfnjoinj

--- stocks.py
+++ stocks.py
@@ -4,7 +4,10 @@ import re
 import os
 import yfinance as yf
 from colorama import Fore
+from datetime import datetime
+import pytz
 import logging
+import sys
 
 logging.getLogger('yfinance').disabled = True
 
@@ -26,14 +29,20 @@ dick = {}
 period = 10
 last_printed = 0
 changed = False
+votes = {}
 
 while True:
 	resp = sock.recv(2048).decode('utf-8')
-	#remove values older than 10 mins
+	if resp == "":
+		sock = socket.socket()
+		sock.connect((server, port))
+		sock.send(f"PASS {token}\n".encode('utf-8'))
+		sock.send(f"NICK {nickname}\n".encode('utf-8'))
+		sock.send(f"JOIN {channel}\n".encode('utf-8'))
+		continue
 	now = time.time()
-	tenago = now - 600
 	for tickler in dick:
-		dick[tickler] = [ts for ts in dick[tickler] if tenago < ts]
+		dick[tickler] = [ts for ts in dick[tickler] if now - 600 < ts]
 	for word in resp.split():
 		if "$" in word:
 			changed = True
@@ -44,13 +53,13 @@ while True:
 				dick[tickler] = [time.time()]
 	
 
-	if now - last_printed > period and changed:
+	if now - last_printed > period and changed == True:
 		changed = False
 		last_printed = now
 		votes = {tickler: len(dick[tickler]) for tickler in dick}
 		votes = dict(sorted(votes.items(), key=lambda x:x[1], reverse=True))
-		os.system('clear')
-		print()
+		print(Fore.YELLOW)
+		print("--{}--".format(datetime.now(pytz.timezone('US/Eastern')).strftime("%Y-%m-%d %H:%M:%S")))
 		printed = 0
 		for tickler in votes:
 			if printed >= 10:
@@ -59,11 +68,17 @@ while True:
 				quote = yf.Ticker(tickler).info
 				price = (quote['bid'] + quote['ask']) / 2
 				percent_change =100 * (price - quote['previousClose'])/ quote['previousClose']
+				print("|", end="")
 				if percent_change > 0:
 					print(Fore.GREEN, end="")
 				else:
 					print(Fore.RED, end="")
-				print(" {:5} ${:<7.2f} {:<+3.2f}%".format(tickler, price, percent_change))
+				print("{:5} ${:<7.2f} {:<+3.2f}%".format(tickler, price, percent_change), end=Fore.YELLOW)
+				print("|")
 				printed += 1
 			except:
-				pass
+				del dick[tickler]
+		for x in range(10 - printed):
+			print('|                     |')
+		print(Fore.YELLOW, end="")
+		print('-----------------------')
