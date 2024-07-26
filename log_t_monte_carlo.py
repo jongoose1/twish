3e0150180074fcc2b448c4ad89501cc7465c409b
Jon Goose <jongoose69@gmail.com>
2024-07-26 12:32:24 -0700

    t tt

--- log_t_monte_carlo.py
+++ log_t_monte_carlo.py
@@ -8,7 +8,7 @@ mu=0.051494
 tau=0.645380
  
 days = 30
-trials = 100000
+trials = 10000
 final_prices = []
 for x in range(trials): 
 	prices = [100] * days
@@ -44,10 +44,52 @@ t_pdf = stats.t.pdf(bin_centers, df, loc, scale)
 ts = t_pdf*bin_width*len(final_prices)
 plt.plot(bin_centers, ts, color='green', linewidth=1)
 
-print('    bin actual          t     cauchy     normal')
-print("\n".join("{:>7.2f} {:>6} {:>10.3f} {:>10.3f} {:>10.3f}"
-	.format(bin_center, int(actual), t, cauchy, normal)
-	for bin_center, actual, t, cauchy, normal
-	in zip(bin_centers, actuals, ts, cauchies, normals)))
+#levy fit
+loc, scale = stats.levy.fit(final_prices)
+print("Levy fit: LOC={:.6f}, SCALE={:.6f}".format(loc, scale))
+levy_pdf = stats.levy.pdf(bin_centers, loc, scale)
+levies = levy_pdf*bin_width*len(final_prices)
+plt.plot(bin_centers, levies, color='purple', linewidth=1)
+
+#alpha fit
+a, loc, scale = stats.alpha.fit(final_prices)
+print("Alpha fit: ALPHA={:.6f}, LOC={:.6f}, SCALE={:.6f}".format(a, loc, scale))
+alpha_pdf = stats.alpha.pdf(bin_centers, a, loc, scale)
+alphas = alpha_pdf*bin_width*len(final_prices)
+plt.plot(bin_centers, alphas, color='orange', linewidth=1)
+
+#beta fit
+a, b, loc, scale = stats.beta.fit(final_prices)
+print("Beta fit: ALPHA={:.6f}, BETA={:.6f}, LOC={:.6f}, SCALE={:.6f}".format(a, b, loc, scale))
+beta_pdf = stats.beta.pdf(bin_centers, a, b, loc, scale)
+betas = beta_pdf*bin_width*len(final_prices)
+plt.plot(bin_centers, betas, color='blue', linewidth=1)
+
+#lognorm dist
+s, loc, scale = stats.lognorm.fit(final_prices)
+print("Log-normal fit: S={:.6f}, LOC={:.6f}, SCALE={:.6f}".format(s, loc, scale))
+lognorm_pdf = stats.lognorm.pdf(bin_centers, s, loc, scale)
+lognormals = lognorm_pdf*bin_width*len(final_prices)
+plt.plot(bin_centers, lognormals, color='red', linewidth=1)
+
+#powerlognorm dist
+c, s, loc, scale = stats.powerlognorm.fit(final_prices)
+print("Power-Log-normal fit: C={:.6f}, S={:.6f}, LOC={:.6f}, SCALE={:.6f}".format(c, s, loc, scale))
+powerlognorm_pdf = stats.powerlognorm.pdf(bin_centers, c, s, loc, scale)
+powerlognormals = powerlognorm_pdf*bin_width*len(final_prices)
+plt.plot(bin_centers, powerlognormals, color='red', linewidth=1)
+
+#skewnormal dist
+a, loc, scale = stats.skewnorm.fit(final_prices)
+print("skewed-normal fit: A={:.6f}, LOC={:.6f}, SCALE={:.6f}".format(a, loc, scale))
+skewnorm_pdf = stats.skewnorm.pdf(bin_centers, a, loc, scale)
+skewnormals = skewnorm_pdf*bin_width*len(final_prices)
+plt.plot(bin_centers, skewnormals, color='red', linewidth=1)
+
+print('    bin actual          t     cauchy     normal      alpha       beta  lognormal powlognorm skewnormal')
+print("\n".join("{:>7.2f} {:>6} {:>10.3f} {:>10.3f} {:>10.3f} {:>10.3f} {:>10.3f} {:>10.3f} {:>10.3f} {:>10.3f}"
+	.format(bin_center, int(actual), t, cauchy, normal, alpha, beta, lognormal, powerlognormal, skewnormal)
+	for bin_center, actual, t, cauchy, normal, alpha, beta, lognormal, powerlognormal, skewnormal
+	in zip(bin_centers, actuals, ts, cauchies, normals, alphas, betas, lognormals, powerlognormals, skewnormals)))
 
 #plt.show()
